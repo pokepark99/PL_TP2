517cea611ecf1379545595c7246330d526596b9d
Sofia <a25991@alunos.ipca.pt>
2024-06-05 18:25:11 +0100

    atualizacao da main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from gramar import Grammar
 
 # se recebeu o nome do ficheiro fca
 if len(sys.argv) == 2:
@@ -12,9 +13,13 @@ if len(sys.argv) == 2:
     try:
         with open (caminho, 'r') as ficheiro:
             conteudo = ficheiro.read()
-
-            # isto e para testar se esta a ler o ficheiro
-            print(conteudo)
+            # isto era para testar se estava a ler o ficheiro
+            #print(conteudo)
+        ag = Grammar()
+        ag.build()
+        # a analise sintatica recebe o conteudo do ficheiro afc
+        res = ag.parse(conteudo)
+        print(f"Resultado: {res}")
 
     # no caso do ficheiro nao ser encontrado
     except FileNotFoundError:
